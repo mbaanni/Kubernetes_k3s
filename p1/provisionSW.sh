3a517c04482b0c1562d257ab91fbaff7409c05c8
Mohamed Baanni <mbaanni@c2r6p9.1337.ma>
2026-02-07 18:19:12 +0100

    set ip for k3s internal-ip

--- p1/provisionSW.sh
+++ p1/provisionSW.sh
@@ -7,4 +7,4 @@ cp /home/vagrant/vagrant_data/id_rsa /home/vagrant/.ssh/id_rsa
 cat /home/vagrant/vagrant_data/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
 
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN="$(cat /home/vagrant/vagrant_data/token)" sh -s
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" K3S_TOKEN="$(cat /home/vagrant/vagrant_data/token)" sh -s
